18c542981b1e9a98ff3a90c238c7a30a010a66fc
eufrat <lukzyw@gmail.com>
2025-09-22 16:57:38 +0200

    Init frontend (angular) backend (spring)

---
+++ quiz-back/src/main/java/com/eufrat/QuizApplication.java
@@ -0,0 +1,9 @@
+package com.eufrat;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizApplication {
+    public static void main(String[] args) {
+    }
+}
