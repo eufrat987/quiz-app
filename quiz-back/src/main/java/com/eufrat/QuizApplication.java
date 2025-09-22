4d0213d65076fffca608f324fb7bc37dd754f15c
eufrat <lukzyw@gmail.com>
2025-09-22 17:15:10 +0200

    Add - postgres bd [docker] - 2 modules [backend] - header [front]

--- quiz-back/src/main/java/com/eufrat/QuizApplication.java
+++
@@ -1,9 +0,0 @@
-package com.eufrat;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class QuizApplication {
-    public static void main(String[] args) {
-    }
-}
